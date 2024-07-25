06d76e69668b9d082a3b4e86eb67338a6a2798b7
Ethan Ankadu <ethan.ankadu@gamesglobal.com>
2024-07-25 15:04:39 +0200

    Added new route for yearly and all data

--- main.py
+++ main.py
@@ -23,6 +23,18 @@ def about(station, date):
             "temperature": temperature
         }
 
+@app.route("/api/v1/<station>")
+def all_data(station):
+    df = pd.read_csv("data_small/TG_STAID" + str(station).zfill(6) + ".txt", skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    df = pd.read_csv("data_small/TG_STAID" + str(station).zfill(6) + ".txt", skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True, port=4000)
